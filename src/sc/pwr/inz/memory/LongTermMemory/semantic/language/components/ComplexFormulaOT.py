21888b88c0c9d2d75b135c513aa3698d1bb55e9e
Jarema27 <jaremaradom@gmail.com>
2017-08-21 16:45:37 +0200

    CFOT and tests,one is broken

---
+++ src/sc/pwr/inz/memory/LongTermMemory/semantic/language/components/ComplexFormulaOT.py
@@ -0,0 +1,76 @@
+"""
+Don't want to destroy ComplexFormula,which was done exclusively for two traits sentences ,which after some time seems to
+be overly precise thing to do,this class will focus on two Object Types/Individual Models questions, this time will
+attempt to make it more flexible for future usage.
+"""
+#   todo Tense
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.State import State
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.Formula import Formula, TypeOfFormula
+
+
+class ComplexFormulaOT(Formula):
+
+    def get_traits(self):
+        pass
+
+    def get_model(self):
+        pass
+
+    def __init__(self, subjects, states, lo, tense=None):
+        """
+        :param subjects list(IndividualModel): or list(Object Type): as subjects
+        :param states list(State):  list of two states,in order in which we want to form formula
+        :param log (LogicalOperator): LO used to connect two parts of Formula
+        :param tense:
+        """
+        if not all(isinstance(elem, State) for elem in states):
+            raise TypeError("Given states aren't instance of State List")
+        if subjects is not None and lo is not None:
+            self.subjects = subjects
+            self.state = states
+            self.LO = lo
+            self.tense = tense
+        else:
+            raise Exception("Obligatory fields include variables with None value")
+
+    def get_states(self):
+        """
+        :return list(State): list of states of formula
+        """
+        return self.state
+
+    def get_logical_operator(self):
+        """
+        :return (LogicalOperator): used to form the formula
+        """
+        return self.LO
+
+    def get_subjects(self):
+        """
+        :return (IndividualModel): IM used to form formula
+        """
+        return self.subjects
+
+    def get_complementary_formulas(self):
+        """
+        Complementary formulas are all formulas which could be acquired from given set of IM,traits and Logical operator
+        :return list(Formula): mind the order
+        """
+        return [ComplexFormulaOT(self.subjects, [State.IS, State.IS], self.LO, self.tense),
+                ComplexFormulaOT(self.subjects, [State.IS, State.IS_NOT], self.LO, self.tense),
+                ComplexFormulaOT(self.subjects, [State.IS_NOT, State.IS], self.LO), self.tense,
+                ComplexFormulaOT(self.subjects, [State.IS_NOT, State.IS_NOT], self.LO, self.tense)]
+
+    @staticmethod
+    def get_type():
+        """
+        :return (TypeOfFormula): Returns type of formula, Complex Formula at this point.
+        """
+        return TypeOfFormula.CF
+
+    def __eq__(self, other):
+        return self.get_states() == other.get_states() and self.get_logical_operator() == other.get_logical_operator() \
+               and self.get_subjects() == other.get_subjects()
+
+    def __hash__(self):
+        return hash(self.state[0]) * sum(list((hash(x) for x in self.subjects)))
---
+++ tests/sc/pwr/inz/memory/LongTermMemory/semantic/language/components/ComplexFormulaOT.py
@@ -0,0 +1,51 @@
+import unittest
+
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.ComplexFormulaOT import ComplexFormulaOT
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.LogicalOperator import LogicalOperator
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.IdentifyingMetaCognition.ObjectType import ObjectType
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.State import State
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.Trait import Trait
+from src.sc.pwr.inz.memory.LongTermMemory.semantic.language.components.Formula import TypeOfFormula
+
+
+class ComplexFormulaOTTest(unittest.TestCase):
+
+    def setUp(self):
+        self.traits = [Trait("Obly"), Trait("Krasny"), Trait("Sowiecki")]
+        self.object_type = ObjectType(1, self.traits)
+        self.traits2 = [Trait("Barowalny"), Trait("Konieczny"), Trait("Bolszoj")]
+        self.object_type2 = ObjectType(2, self.traits2)
+
+        self.s1 = State.IS
+        self.s2 = State.IS_NOT
+        self.s3 = State.MAYHAPS
+
+        self.cf1 = ComplexFormulaOT([self.object_type, self.object_type2], [self.s2, self.s3], LogicalOperator.AND)
+        self.cf2 = ComplexFormulaOT([self.object_type, self.object_type2], [self.s1, self.s2], LogicalOperator.OR)
+
+    def test_get_states(self):
+        self.assertEqual(self.cf1.get_states(), [self.s2, self.s3])
+        self.assertEqual(self.cf2.get_states(), [self.s1, self.s2])
+
+    def test_get_model(self):
+        self.assertEqual(self.cf1.get_subjects(), [self.object_type, self.object_type2])
+        self.assertEqual(self.cf2.get_subjects(), [self.object_type, self.object_type2])
+
+    def test_get_type(self):
+        self.assertEqual(self.cf1.get_type(), TypeOfFormula.CF)
+        self.assertEqual(self.cf2.get_type(), TypeOfFormula.CF)
+
+    def test_get_complementary_formulas(self):
+        self.assertEqual(self.cf1.get_complementary_formulas(), [ComplexFormulaOT([self.object_type, self.object_type2],
+                                                                                  [self.s1, self.s1], LogicalOperator.
+                                                                                  AND)
+                         , ComplexFormulaOT([self.object_type, self.object_type2], [self.s1, self.s2], LogicalOperator.
+                                            AND)
+                         , ComplexFormulaOT([self.object_type, self.object_type2], [self.s2, self.s1], LogicalOperator.
+                                            AND)
+                         , ComplexFormulaOT([self.object_type, self.object_type2], [self.s2, self.s2], LogicalOperator.
+                                            AND)])
+
+    def test_eq(self):
+        self.assertEqual(self.cf1, self.cf1)
+        self.assertNotEqual(self.cf1, self.cf2)
